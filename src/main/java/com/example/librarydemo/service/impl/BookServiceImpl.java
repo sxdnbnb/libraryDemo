1b9a60a68bc7a84fe564891a0c95fae82f8de4fe
sxdnbnb <1204160604@qq.com>
2024-06-17 20:27:39 +0800

    增加redis

--- src/main/java/com/example/librarydemo/service/impl/BookServiceImpl.java
+++ src/main/java/com/example/librarydemo/service/impl/BookServiceImpl.java
@@ -1,15 +1,22 @@
 package com.example.librarydemo.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.librarydemo.dto.Result;
 import com.example.librarydemo.entity.Book;
 import com.example.librarydemo.mapper.BookMapper;
 import com.example.librarydemo.service.IBookService;
-import lombok.extern.slf4j.Slf4j;
+
+import jakarta.annotation.Resource;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static com.example.librarydemo.utils.RedisConstants.*;
 
 /**
  * @version 1.0
@@ -20,14 +27,39 @@ import java.util.List;
 // @Slf4j
 @Service
 public class BookServiceImpl extends ServiceImpl<BookMapper, Book> implements IBookService {
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
+    // 使用redis缓存
     @Override
     public Result queryBookById(Long id) {
-        Book book = getById(id);
-        if (book == null){
-            return Result.fail("图书不存在");
+        String key = CACHE_Book_KEY + id;
+        // 1.从redis查询书籍缓存
+        String bookJson = stringRedisTemplate.opsForValue().get(key);
+        // 2.判断是否存在
+        if (StrUtil.isNotBlank(bookJson)) {
+            // 3.存在，直接返回
+            Book book = JSONUtil.toBean(bookJson, Book.class);
+            return Result.ok(book);
         }
-        // log.debug("图书id为：{}", id);
+        // 判断命中的是否是空值
+        if (bookJson != null) {
+            // 返回一个错误信息
+            return Result.fail("图书信息不存在！");
+        }
+
+        // 4.不存在，根据id查询数据库
+        Book book = getById(id);
+        // 5.不存在，返回错误
+        if (book == null) {
+            // 将空值写入redis
+            stringRedisTemplate.opsForValue().set(key, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
+            // 返回错误信息
+            return Result.fail("图书不存在！");
+        }
+        // 6.存在，写入redis
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(book), CACHE_Book_TTL, TimeUnit.MINUTES);
+        // 7.返回
         return Result.ok(book);
     }
 
@@ -65,8 +97,10 @@ public class BookServiceImpl extends ServiceImpl<BookMapper, Book> implements IB
         if (id == null) {
             return Result.fail("图书id不能为空！");
         }
-        // 更新数据库
+        // 1.更新数据库
         updateById(book);
+        // 2.删除缓存
+        stringRedisTemplate.delete(CACHE_Book_KEY + id);
         return Result.ok();
     }
 
@@ -89,8 +123,10 @@ public class BookServiceImpl extends ServiceImpl<BookMapper, Book> implements IB
         if (id == null) {
             return Result.fail("图书不存在");
         }
-        // 删除数据库
+        // 1.删除数据库
         removeById(book);
+        // 2.删除缓存
+        stringRedisTemplate.delete(CACHE_Book_KEY + id);
         return Result.ok();
     }
 }
---
+++ src/main/java/com/example/librarydemo/utils/RedisConstants.java
@@ -0,0 +1,14 @@
+package com.example.librarydemo.utils;
+
+/**
+ * @version 1.0
+ * @description: TODO
+ * @author: 12041
+ * @date: 2024/6/14 19:44
+ */
+public class RedisConstants {
+    public static final Long CACHE_Book_TTL = 30L;
+    public static final String CACHE_Book_KEY = "cache:book:";
+
+    public static final Long CACHE_NULL_TTL = 2L;
+}
